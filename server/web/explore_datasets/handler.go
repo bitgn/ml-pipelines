1150c063fd17a216d12ce6cf8e218ef53cd8b686
Rinat Abdullin <rinat.abdullin@gmail.com>
2019-08-09 19:29:16 +0500

    Implement dataset search

--- server/web/explore_datasets/handler.go
+++ server/web/explore_datasets/handler.go
@@ -6,6 +6,7 @@ import (
 	"mlp/catalog/domain"
 	"mlp/catalog/web"
 	"net/http"
+	"strings"
 )
 
 
@@ -23,6 +24,55 @@ type Model struct {
 }
 
 
+
+func contains(source, lowercaseSubstring string) bool{
+	return strings.Contains(strings.ToLower(source), lowercaseSubstring)
+}
+
+func datasetMatchesOne(meta *Dataset, query string) bool {
+	if query == "stale"  && meta.IsStale{
+			return true
+	}
+
+	if contains(meta.ProjectName, query) {
+		return true
+	}
+	ds := meta.Dataset
+	if contains(ds.Name, query){
+		return true
+	}
+	if contains(ds.DataFormat, query){
+		return true
+	}
+	if contains(ds.Description, query){
+		return true
+	}
+	if ds.Sample != nil && contains(string(ds.Sample.Body), query){
+		return true
+	}
+
+
+
+
+
+	return false
+}
+
+func datasetMatchesQuery(ds *Dataset, query []string) bool {
+
+	if len(query) == 0{
+		return true
+	}
+
+	for _, l := range query {
+		if !datasetMatchesOne(ds, l){
+			return false
+		}
+	}
+	return true
+}
+
+
 func Handle(env *db.DB, w http.ResponseWriter, query string){
 	tx := env.MustRead()
 
@@ -33,16 +83,25 @@ func Handle(env *db.DB, w http.ResponseWriter, query string){
 	t := template.New("layout")
 
 
+	query = strings.ToLower(strings.TrimSpace(query))
+
+	lines := strings.Split(query, " ")
 
 	var metas []*Dataset
 
 	for _,ds := range datasets{
+		prj := db.GetProject(tx, ds.ProjectId)
 		meta := &Dataset{
 			Dataset:ds,
 			IsStale:domain.IsStale(ds),
+			ProjectName:prj.Name,
 		}
 
-		metas = append(metas, meta)
+
+
+		if datasetMatchesQuery(meta, lines) {
+			metas = append(metas, meta)
+		}
 	}
 
 
